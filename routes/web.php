12dffb95f1e73ea11b294d0b893a58838c1bc556
zidankhan86 <zidankhan718@gmail.com>
2023-10-24 02:09:32 +0600

    cdn , components

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Livewire\PostComponent;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/post',PostComponent::class)->name('post');
